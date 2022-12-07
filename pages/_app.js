e215671ddad410bf0cb6a3eac3acfeb45b3359c4
sugam7842692@gmail.com <sugamkuber@gmail.com>
2022-12-07 23:15:56 +0530

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout className="back">
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
